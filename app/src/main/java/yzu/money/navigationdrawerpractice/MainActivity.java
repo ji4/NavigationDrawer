0326b2874159cd5864d7310dd6e69ccd84db4c89
howdyhi <money@15-17.wlan.yzu.edu.tw>
2016-10-31 15:40:24 +0800

    1st

---
+++ app/src/main/java/yzu/money/navigationdrawerpractice/MainActivity.java
@@ -0,0 +1,14 @@
+package yzu.money.navigationdrawerpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Drae
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
